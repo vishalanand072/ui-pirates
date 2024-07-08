9f9d468639371893258c1194d59d8f599d831d3a
DanishAnsari5522 <ddanish1928@gmail.com>
2024-07-08 16:15:58 +0530

    add smothe ScrollTrigger and make video carosal

---
+++ screens/Home/animation.tsx/HeroImage.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React, { useLayoutEffect, useRef } from "react";
+import styles from "./style.module.css";
+import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Index() {
+  const background = useRef(null);
+  const introImage = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 1,
+        start: "top",
+        end: "+=500px",
+      },
+    });
+
+    timeline.fromTo(
+      background.current,
+      {
+        width: "70%",
+        display: "flex",
+        justifyContent: "center",
+        objectFit: "fill",
+      },
+      { width: "100%" }
+    );
+    // .to(introImage.current, { height: "200px" }, 0);
+  }, []);
+
+  return (
+    <div className={styles.homeHeader}>
+      <div className={styles.intro}>
+        <h1 className="text-4xl md:text-5xl font-bold mb-4 text-white">
+          Hero Section Heading
+        </h1>
+        <p className="text-lg md:text-xl max-w-xl mx-auto">
+          This is a paragraph that describes the content of the hero section.
+          Scroll down to see the effect on the video.
+        </p>
+        <p className="text-lg md:text-xl max-w-xl mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+          fuga itaque ut beatae alias excepturi adipisci at explicabo
+          voluptates, consectetur maxime enim vitae id obcaecati ex, eius eos ad
+          corrupti!
+        </p>
+        <p className="text-lg md:text-xl max-w-xl mx-auto test-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+          fuga itaque ut beatae alias excepturi adipisci at explicabo
+          voluptates, consectetur maxime enim vitae id obcaecati ex, eius eos ad
+          corrupti!
+        </p>
+      </div>
+      <div className={styles.backgroundImage} ref={background}>
+        <video className={styles.video} muted loop>
+          <source
+            src="https://www.w3schools.com/html/mov_bbb.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+}
--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -3,13 +3,21 @@ import HomeCard from "./card";
 import Hero from "./animation.tsx/Hero";
 // import ScrollAnimation from "./animation.tsx/rightScroll";
 import ScrollSection from "./animation.tsx/rightScroll";
+import VideoCarousel from "./animation.tsx/videoCarosal";
+import DivScroll from "./animation.tsx/divScroll";
+import HeroImage from "./animation.tsx/HeroImage";
+import Dan from "./animation.tsx/Descriotion";
 const HomePage = () => {
   return (
     <>
-      {/* <HomeHero />
-            <HomeCard /> */}
-      <Hero />
+      <HeroImage />
+      {/* <Hero /> */}
+
+      {/* <Dan /> */}
       <ScrollSection />
+
+      <VideoCarousel />
+      {/* <DivScroll /> */}
     </>
   );
 };
