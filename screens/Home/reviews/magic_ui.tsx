077ac26902ee245b30af9ccd62ebc37dc44e1d24
deonmenezes <deonmenezescodes@gmail.com>
2024-07-09 09:52:39 +0530

    updated the cards , made all the major changes , color of rea land ui pirates need to be changed also cards need to be opaque

--- screens/Home/reviews/magic_ui.tsx
+++ screens/Home/reviews/magic_ui.tsx
@@ -4,7 +4,7 @@ import ReviewCard from "./card";
 export function MagicCardDemo() {
   const { theme } = useTheme();
   const gradientColor = theme === "dark" ? "#262626" : "#D9D9D955";
-  const data= [1,2,3,4,5,6,7,8,9]
+  const data= [1,2,3,4,5,6]
 
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
