33581e034898429d86980a06143f6e9fde51bf82
Sanah Shrivastav <sanahshrivastav@gmail.com>
2024-07-10 10:11:24 +0530

    create about-aipirate folder

---
+++ screens/Home/MinimalInvolvementCard.tsx
@@ -0,0 +1,28 @@
+/*
+import { Card, Spacer, Text } from "@nextui-org/react";
+
+const MinimalInvolvementCard: React.FC = () => {
+  return (
+    <Card css={{ maxWidth: "400px", padding: "$6", boxShadow: "$sm" }}>
+      <Card.Header css={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <video
+          alt="Example header video"
+          src="https://www.pexels.com/video/drops-of-liquid-on-a-tray-to-be-use-as-samples-3195394/" 
+          style={{ borderRadius: "10px", width: "100%" }}
+        />
+        <Spacer y={1} />
+        <Text h3 css={{ textAlign: "center", marginBottom: 0 }}>
+          We Excel with Minimal Client Involvement
+        </Text>
+      </Card.Header>
+      <Card.Body css={{ paddingY: "$2" }}>
+        <Text css={{ textAlign: "center" }}>
+          Our efficient workflows ensure minimal client interaction, allowing you to focus on your business while we deliver outstanding results.
+        </Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default MinimalInvolvementCard;
+*/
--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,5 +1,15 @@
 import HomeHero from "./hero";
 import HomeCard from "./card";
+import MinimalInvolvementCard from "./MinimalInvolvementCard";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <MinimalInvolvementCard />
+    </div>
+  );
+}
+
 const HomePage = () => {
     return (
         <>
