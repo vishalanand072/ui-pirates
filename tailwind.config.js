9c48170941e0f306d83a1e53fbb39fa6a3ef39b6
Sanah Shrivastav <sanahshrivastav@gmail.com>
2024-07-18 18:01:33 +0530

    aboutCard update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
+    extend: {animation: {
+      meteor: "meteor 5s linear infinite",
+    },
+    keyframes: {
+      meteor: {
+        "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+        "70%": { opacity: 1 },
+        "100%": {
+          transform: "rotate(215deg) translateX(-500px)",
+          opacity: 0,
+        },
+      },
+    },
       fontFamily: {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-geist-mono)"],
