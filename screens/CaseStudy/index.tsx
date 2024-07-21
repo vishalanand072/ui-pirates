3cf4cf2259d404b2bc19ccbc149a2a7d094e9a72
Sanah Shrivastav <sanahshrivastav@gmail.com>
2024-07-21 21:19:52 +0530

    CaseStudy update

--- screens/CaseStudy/index.tsx
+++ screens/CaseStudy/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import XperitiCard from './Xperiti';
-
-function Xperiti() {
-  return (
-    <div className="App">
-      <XperitiCard title="Xperiti" subtitle="An Ipsos Company" imageUrl="https://www.figma.com/design/0PiY8LYUZG9LxqI948IWaf/UI-Pirates?node-id=1429-1823&t=fyDW4QQBUVh8dM2P-4" />
-
-      <XperitiCard />
-    </div>
-  );
+import Overview from "./Overview";
+import Xperiti from "./Xperiti";
+const Casestudy = () => {
+    return ( 
+        <div>
+            <Overview></Overview>
+            <Xperiti></Xperiti>
+        </div>
+     );
 }
-
-export default Xperiti;
+ 
+export default Casestudy ;
