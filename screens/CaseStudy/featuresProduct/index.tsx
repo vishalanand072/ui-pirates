77e711816287ee750746be20dbf770034c60bda0
DanishAnsari5522 <ddanish1928@gmail.com>
2024-08-06 17:37:06 +0530

    add tabs

--- screens/CaseStudy/featuresProduct/index.tsx
+++ screens/CaseStudy/featuresProduct/index.tsx
@@ -2,13 +2,15 @@ import AnimationProduct from "./animation";
 const FeaturesProduct = () => {
   return (
     <>
-      <div className="container mx-auto px-20 max-lg:px-16 max-md:px-12">
-        <div className="heading-comp">
-          <h1 className="text-black heading">Features & Product</h1>
-          <span className="text-gray-300 text-6xl font-light">06</span>
+      <div className=" bg-secondary-900 py-12">
+        <div className="container mx-auto px-20 max-lg:px-16 max-md:px-12">
+          <div className="heading-comp">
+            <h1 className="text-white heading">Features & Product</h1>
+            <span className="text-gray-300 text-6xl font-light">06</span>
+          </div>
         </div>
+        <AnimationProduct />
       </div>
-      <AnimationProduct />
     </>
   );
 };
--- screens/CaseStudy/hero/index.tsx
+++ screens/CaseStudy/hero/index.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const CaseStudyHero = () => {
   return (
     <>
-      <div className="h-screen bg-black  object-cover -mt-16">
-        <Image src={hero} alt="Client" className="w-[70%]" />
+      <div className="bg-black  -mt-16">
+        <Image src={hero} alt="Client" className="w-[100vw] h-[100vh]" />
       </div>
     </>
   );
