5d703ae9f7e1b911e007540663179d5a6990f259
DanishAnsari5522 <ddanish1928@gmail.com>
2024-08-19 15:12:54 +0530

    comment the all console.log

--- screens/CaseStudyComp/index.tsx
+++ screens/CaseStudyComp/index.tsx
@@ -61,7 +61,7 @@ import StickyTabs from "./tabs";
 import WeCreate from "../Home/weCreate";
 
 const Casestudy = (props: any) => {
-  console.log(props.data[7]);
+  // console.log(props.data[7]);
 
   return (
     <div>
